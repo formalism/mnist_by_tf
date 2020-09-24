350efd1af97c27b652aa178b9eda7e951dea2c5b
Tomoyuki Ueda <tu@formalism.net>
2020-09-24 13:48:17 +0900

    Add training program in Python by TF

---
+++ mnist.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+
+# from tensorflow.org/tutorials/quickstart/beginner?hl=ja
+# and tensorflow.org/tutorials/keras/save_and_load?hl=ja 
+
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(10)
+])
+
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+model.compile(optimizer='adam', loss=loss_fn, metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=5)
+
+model.evaluate(x_test, y_test, verbose=2)
+
+probability_model = tf.keras.Sequential([ model, tf.keras.layers.Softmax() ])
+probability_model.save('saved_model')
+
